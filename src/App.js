dc9717a97d4f7bc9b5906a289d61365fc7de6372
ebarroso2214 <ebarroso2214@gmail.com>
2023-04-12 21:19:47 -0400

    Declarative Counter, when button is pressed, counter goes up by one or down by one but stops at 0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  //state, function to update state
+  const [counter, setCounter] = useState(0) 
+  //DO NOT EVER MODIFY STATE DIRECTLY
+  //ex. counter = 10, it will throw the virtual DOM out of sync. 
+  
+  //Name things appropriately, remember to use set[name] as the second item in the array. setCounter is a function that modifies the counter in this case
+
+  //Concept: State, state is the data in your application that changes, and is tracked, you need the DOM to reflect that change
+  const increment = ()=>{
+    setCounter(currentValue => currentValue + 1)
+  }
+
+  const decrement = ()=>{
+    if (counter === 0){return}
+    setCounter(currentValue => currentValue - 1)
+  }
+  
+  const me = 'Esteban'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Count: {counter}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decerement</button>
     </div>
   );
 }
